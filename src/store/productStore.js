76fb9881b0f1abc8ae9d4cc69ea541b481e05a5f
vladimir bejarano cordoba <sefiro25@hotmail.com>
2026-05-11 00:54:16 -0500

    Paso 5: Implementación de acciones de fetching en productStore.js

--- src/store/productStore.js
+++ src/store/productStore.js
@@ -1,11 +1,20 @@
 import { create } from "zustand";
+import { getProducts } from "../services/productService";
 
 const useProductStore = create((set) => ({
   products: [],
   loading: false,
   error: null,
   
-  // Aquí irán las acciones pronto...
+  fetchProducts: async () => {
+    set({ loading: true, error: null });
+    try {
+      const data = await getProducts();
+      set({ products: data, loading: false });
+    } catch (err) {
+      set({ error: "Error al cargar productos", loading: false });
+    }
+  },
 }));
 
 export default useProductStore;
